5c0f871a4e43713d2c86dcd74a2a0f46745da6d1
manjit.shakya <manjit.shakya@f1soft.com>
2020-06-25 07:44:33 +0545

    changes

---
+++ src/main/java/com/mnzit/learner/spring/spel/demospel/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.mnzit.learner.spring.spel.demospel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Manjit Shakya
+ * @email manjit.shakya@f1soft.com
+ */
+@RestController
+@RequestMapping("student")
+public class StudentController {
+
+}
---
+++ src/main/java/com/mnzit/learner/spring/spel/demospel/spel/playground/SpelTesting.java
@@ -0,0 +1,8 @@
+package com.mnzit.learner.spring.spel.demospel.spel.playground;
+
+/**
+ * @author Manjit Shakya
+ * @email manjit.shakya@f1soft.com
+ */
+public class SpelTesting {
+}
